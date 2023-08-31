869b5a042b21c9d773d17816207cf37c18351ef0
Pooja Reddy Nathala <poojardy@amazon.com>
2023-08-31 09:29:48 -0400

    inital setup for terraform execution

---
+++ integration-tests/terraform/common/output.tf
@@ -0,0 +1,35 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+output "testing_id" {
+  value = random_id.testing_id.hex
+}
+
+output "cwa_iam_role" {
+  value = "cwa-e2e-iam-role"
+}
+
+output "cwa_onprem_assumed_iam_role_arm" {
+  value = "arn:aws:iam::506463145083:role/CloudWatchAgentServerRoleOnPrem"
+}
+
+output "cwa_iam_policy" {
+  value = "cwa-e2e-iam-policy"
+}
+
+output "cwa_iam_instance_profile" {
+  value = "cwa-e2e-iam-instance-profile"
+}
+
+output "cwagent_image_repo" {
+  value = var.cwagent_image_repo
+}
+
+output "cwagent_image_tag" {
+  value = var.cwagent_image_tag
+}
+
+output "vpc_security_group" {
+  value = var.vpc_security_group
+}
+
---
+++ integration-tests/terraform/eks/daemon/variables.tf
@@ -0,0 +1,32 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+variable "region" {
+  type    = string
+  default = "us-west-2"
+}
+
+variable "cwagent_image_repo" {
+  type    = string
+  default = "public.ecr.aws/cloudwatch-agent/cloudwatch-agent"
+}
+
+variable "cwagent_image_tag" {
+  type    = string
+  default = "latest"
+}
+
+variable "k8s_version" {
+  type    = string
+  default = "1.24"
+}
+
+variable "ami_type" {
+  type    = string
+  default = "AL2_x86_64"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3a.medium"
+}
