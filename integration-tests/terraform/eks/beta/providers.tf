c728a7e40a18eb02b253144c8a288b444b55588a
Pooja Reddy Nathala <poojardy@amazon.com>
2023-09-01 16:15:16 -0400

    added beta cluster creation changes

---
+++ integration-tests/terraform/eks/beta/providers.tf
@@ -0,0 +1,20 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+provider "aws" {
+  region = var.region
+  endpoints {
+    eks = "https://api.beta.us-west-2.wesley.amazonaws.com"
+  }
+}
+
+provider "kubernetes" {
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.this.name]
+  }
+  host                   = aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
