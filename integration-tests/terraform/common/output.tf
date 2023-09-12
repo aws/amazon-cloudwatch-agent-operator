2249c76f127ab1201535f13297822ea037ddade1
Pooja Reddy Nathala <poojardy@amazon.com>
2023-09-12 11:47:52 -0400

    changed as per review comments

--- integration-tests/terraform/common/output.tf
+++ integration-tests/terraform/common/output.tf
@@ -9,10 +9,6 @@ output "cwa_iam_role" {
   value = "cwa-e2e-iam-role"
 }
 
-output "cwa_onprem_assumed_iam_role_arm" {
-  value = "arn:aws:iam::506463145083:role/CloudWatchAgentServerRoleOnPrem"
-}
-
 output "cwa_iam_policy" {
   value = "cwa-e2e-iam-policy"
 }
