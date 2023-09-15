87db81d7ccfca7447b2f6eebda91e923d6166526
Mitali Salvi <mitsalvi@amazon.com>
2023-09-15 16:06:10 -0400

    apm setup

--- integration-tests/terraform/eks/setup-apm.sh
+++ integration-tests/terraform/eks/setup-apm.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-echo $APM_YAML
+# Install cloudwatch-agent operator
+echo "Creating cloudwatch-agent operator"
+for i in {1..3}
+do
+    operator_status=$(kubectl apply -f $APM_YAML 2>&1)
+    if [[ "${operator_status}" == *"Error "* ]];  then
+        sleep 60
+        continue
+    fi
+    break
+done
+
+sleep 60
+kubectl get pods -A
