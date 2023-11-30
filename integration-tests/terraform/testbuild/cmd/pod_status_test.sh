75c8c077c8d321cea033486cb13820546d11cc18
Bhanu Bandi <bhanuba@amazon.com>
2023-11-29 23:28:36 -0500

    edited yml

--- integration-tests/terraform/testbuild/cmd/pod_status_test.sh
+++ integration-tests/terraform/testbuild/cmd/pod_status_test.sh
@@ -10,7 +10,7 @@ kubectl get pods -A
 pod_name="$(kubectl get pods -n amazon-cloudwatch -l app.kubernetes.io/component=amazon-cloudwatch-agent,app.kubernetes.io/instance=amazon-cloudwatch.cloudwatch-agent -o=jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')"
 status=$(kubectl get pod "$pod_name" -n amazon-cloudwatch -o=jsonpath='{.status.phase}')
 if [ "$status" != "Running" ]; then
-  echo "Pod $pod_name is not running. Exiting with error."
+  echo "Pod $pod_name is not running. Exiting with ERROR."
   exit 1
 else
   echo "Pod $pod_name is running. Continue with the workflow."
