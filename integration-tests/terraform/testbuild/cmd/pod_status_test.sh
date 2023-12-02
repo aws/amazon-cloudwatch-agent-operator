8c08b8f2016364132a12ba51e6893e368b349141
Bhanu Bandi <bhanuba@amazon.com>
2023-12-01 22:16:32 -0500

    edited yml

--- integration-tests/terraform/testbuild/cmd/pod_status_test.sh
+++ integration-tests/terraform/testbuild/cmd/pod_status_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl apply -f ../../../helm/crds/*.yaml
+kubectl apply -f ../../../../helm/crds/*.yaml
 helm install --set manager.image.repository=cwagent-operator-pre-release --set manager.image.tag=latest --set manager.image.repositoryDomainMap.test-us-west-2=506463145083.dkr.ecr.us-west-2.amazonaws.com/cwagent-operator-pre-release ../../../helm
 kubectl apply -f ../sample-instrumentation.yaml
 sleep 10
