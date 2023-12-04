253b5e1f574710ffe99123a9199b8ecfd4faa55c
Bhanu Bandi <bhanuba@amazon.com>
2023-12-04 16:19:10 -0500

    edited yml

--- integration-tests/terraform/testbuild/main.tf
+++ integration-tests/terraform/testbuild/main.tf
@@ -111,7 +111,6 @@ resource "null_resource" "kubectl" {
 resource "null_resource" "run_make_deploy" {
   provisioner "local-exec" {
     command     = "make deploy"
-    working_dir = "../../../Makefile"
   }
 }
 
