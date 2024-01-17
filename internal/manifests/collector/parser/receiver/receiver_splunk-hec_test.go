3fd443d6d3effaa765df360bef6a5c0e45493504
Lisa Guo <lisaguo@amazon.com>
2024-01-17 09:52:11 -0500

    Remove receiver parser except generic

--- internal/manifests/collector/parser/receiver/receiver_splunk-hec_test.go
+++
@@ -1,6 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-package receiver
-
-// all tests for the Splunk Hec parser are currently part of the test TestDownstreamParsers
--- internal/manifests/collector/parser/receiver/receiver_wavefront_test.go
+++
@@ -1,6 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-package receiver
-
-// all tests for the Wavefront parser are currently part of the test TestDownstreamParsers
