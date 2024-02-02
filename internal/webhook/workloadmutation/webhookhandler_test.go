291e27acec271a4b80f6888ccd79cdcd1e0dacf1
Mitali Salvi <mitsalvi@amazon.com>
2024-02-02 11:15:07 -0500

    updating unit tests

--- internal/webhook/workloadmutation/webhookhandler_test.go
+++ internal/webhook/workloadmutation/webhookhandler_test.go
@@ -5,6 +5,13 @@ package workloadmutation
 
 import (
 	"context"
+	"encoding/json"
+	"github.com/stretchr/testify/require"
+	admv1 "k8s.io/api/admission/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"net/http"
 	"testing"
 
@@ -25,35 +32,105 @@ var (
 	logger    = logf.Log.WithName("unit-tests")
 )
 
-func TestInvalidRequest(t *testing.T) {
+func TestHandle(t *testing.T) {
 	for _, tt := range []struct {
-		req                  admission.Request
-		autoAnnotationConfig auto.AnnotationConfig
-		name                 string
-		expected             int32
-		allowed              bool
+		req      admission.Request
+		name     string
+		expected int32
+		allowed  bool
 	}{
 		{
-			name:     "invalid payload",
+			name:     "empty payload",
 			req:      admission.Request{},
 			expected: http.StatusBadRequest,
 			allowed:  false,
-			autoAnnotationConfig: auto.AnnotationConfig{
-				Java: auto.AnnotationResources{
-					Namespaces: []string{"keep-auto-java"},
-				},
-			},
+		},
+		{
+			name: "invalid empty daemon-set payload",
+			req: func() admission.Request {
+				ds := appsv1.DaemonSet{}
+				encoded, err := json.Marshal(ds)
+				require.NoError(t, err)
+
+				return admission.Request{
+					AdmissionRequest: admv1.AdmissionRequest{
+						Namespace: "testing",
+						Object: runtime.RawExtension{
+							Raw: encoded,
+						},
+					},
+				}
+			}(),
+			expected: http.StatusBadRequest,
+			allowed:  false,
+		},
+		{
+			name: "invalid pod payload",
+			req: func() admission.Request {
+				pod := corev1.Pod{}
+				encoded, err := json.Marshal(pod)
+				require.NoError(t, err)
+
+				return admission.Request{
+					AdmissionRequest: admv1.AdmissionRequest{
+						Namespace: "testing",
+						Object: runtime.RawExtension{
+							Raw: encoded,
+						},
+					},
+				}
+			}(),
+			expected: http.StatusBadRequest,
+			allowed:  false,
+		},
+		{
+			name: "valid workload payload",
+			req: func() admission.Request {
+				ds := appsv1.DaemonSet{
+					TypeMeta: metav1.TypeMeta{
+						Kind: "DaemonSet",
+					},
+					Spec: appsv1.DaemonSetSpec{
+						Template: corev1.PodTemplateSpec{
+							ObjectMeta: metav1.ObjectMeta{
+								Annotations: map[string]string{},
+							},
+						},
+					},
+				}
+				encoded, err := json.Marshal(ds)
+				require.NoError(t, err)
+
+				return admission.Request{
+					AdmissionRequest: admv1.AdmissionRequest{
+						Kind: metav1.GroupVersionKind{
+							Kind: "DaemonSet",
+						},
+						Namespace: "testing",
+						Object: runtime.RawExtension{
+							Raw: encoded,
+						},
+					},
+				}
+			}(),
+			expected: http.StatusOK,
+			allowed:  true,
 		},
 	} {
 		t.Run(tt.name, func(t *testing.T) {
 			// prepare
 			cfg := config.New()
 			decoder := admission.NewDecoder(scheme.Scheme)
+			autoAnnotationConfig := auto.AnnotationConfig{
+				Java: auto.AnnotationResources{
+					Namespaces: []string{"keep-auto-java"},
+				},
+			}
 			mutators := auto.NewAnnotationMutators(
 				k8sClient,
 				k8sClient,
 				logr.Logger{},
-				tt.autoAnnotationConfig,
+				autoAnnotationConfig,
 				instrumentation.NewTypeSet(instrumentation.TypeJava),
 			)
 			injector := NewWebhookHandler(cfg, logger, decoder, k8sClient, mutators)
@@ -63,8 +140,10 @@ func TestInvalidRequest(t *testing.T) {
 
 			// verify
 			assert.Equal(t, tt.allowed, res.Allowed)
-			assert.NotNil(t, res.AdmissionResponse.Result)
-			assert.Equal(t, tt.expected, res.AdmissionResponse.Result.Code)
+			if !tt.allowed {
+				assert.NotNil(t, res.AdmissionResponse.Result)
+				assert.Equal(t, tt.expected, res.AdmissionResponse.Result.Code)
+			}
 		})
 	}
 }
