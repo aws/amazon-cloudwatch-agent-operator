4202faee69ab83e3ddc74c15b3c22627ef886576
musa-asad <musaasad@amazon.com>
2024-08-20 18:52:27 -0400

    Revert "Updated config_replace.go to include allocator logic."
    This reverts commit 2c7b788e60ac9ed4db9666e8c8c792e8686b2cf9.

--- internal/manifests/collector/config_replace.go
+++ internal/manifests/collector/config_replace.go
@@ -5,9 +5,6 @@ package collector
 
 import (
 	"encoding/json"
-	ta "github.com/aws/amazon-cloudwatch-agent-operator/internal/manifests/targetallocator/adapters"
-	"github.com/aws/amazon-cloudwatch-agent-operator/internal/naming"
-	"github.com/aws/amazon-cloudwatch-agent-operator/pkg/featuregate"
 
 	_ "github.com/prometheus/prometheus/discovery/install" // Package install has the side-effect of registering all builtin.
 
@@ -16,54 +13,10 @@ import (
 )
 
 func ReplaceConfig(instance v1alpha1.AmazonCloudWatchAgent) (string, error) {
-	// Check if TargetAllocator is enabled, if not, return the original config
-	if !instance.Spec.TargetAllocator.Enabled {
-		return instance.Spec.Config, nil
-	}
-
 	config, err := adapters.ConfigFromJSONString(instance.Spec.Config)
 	if err != nil {
 		return "", err
 	}
-
-	promCfgMap, getCfgPromErr := ta.ConfigToPromConfig(instance.Spec.Config)
-	if getCfgPromErr != nil {
-		return "", getCfgPromErr
-	}
-
-	validateCfgPromErr := ta.ValidatePromConfig(promCfgMap, instance.Spec.TargetAllocator.Enabled, featuregate.EnableTargetAllocatorRewrite.IsEnabled())
-	if validateCfgPromErr != nil {
-		return "", validateCfgPromErr
-	}
-
-	if featuregate.EnableTargetAllocatorRewrite.IsEnabled() {
-		// To avoid issues caused by Prometheus validation logic, which fails regex validation when it encounters
-		// $$ in the prom config, we update the YAML file directly without marshaling and unmarshalling.
-		updPromCfgMap, getCfgPromErr := ta.AddTAConfigToPromConfig(promCfgMap, naming.TAService(instance.Name))
-		if getCfgPromErr != nil {
-			return "", getCfgPromErr
-		}
-
-		// type coercion checks are handled in the AddTAConfigToPromConfig method above
-		config["receivers"].(map[interface{}]interface{})["prometheus"] = updPromCfgMap
-
-		out, updCfgMarshalErr := json.Marshal(config)
-		if updCfgMarshalErr != nil {
-			return "", updCfgMarshalErr
-		}
-
-		return string(out), nil
-	}
-
-	// To avoid issues caused by Prometheus validation logic, which fails regex validation when it encounters
-	// $$ in the prom config, we update the YAML file directly without marshaling and unmarshalling.
-	updPromCfgMap, err := ta.AddHTTPSDConfigToPromConfig(promCfgMap, naming.TAService(instance.Name))
-	if err != nil {
-		return "", err
-	}
-
-	// type coercion checks are handled in the ConfigToPromConfig method above
-	config["receivers"].(map[interface{}]interface{})["prometheus"] = updPromCfgMap
 
 	out, err := json.Marshal(config)
 	if err != nil {
