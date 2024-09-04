cff2d6e7bece543c0c0a568de428c7e4d435def3
musa-asad <musaasad@amazon.com>
2024-09-04 14:51:31 -0400

    Added configmap testing for prometheus.

--- internal/manifests/collector/configmap_test.go
+++ internal/manifests/collector/configmap_test.go
@@ -4,7 +4,14 @@
 package collector
 
 import (
+	"fmt"
+	"os"
 	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/aws/amazon-cloudwatch-agent-operator/apis/v1alpha1"
+	"github.com/aws/amazon-cloudwatch-agent-operator/internal/manifests"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -38,5 +45,55 @@ func TestDesiredConfigMap(t *testing.T) {
 }
 
 func TestDesiredPrometheusConfigMap(t *testing.T) {
-	// TBD
+	expectedLabels := map[string]string{
+		"app.kubernetes.io/managed-by": "amazon-cloudwatch-agent-operator",
+		"app.kubernetes.io/instance":   "default.test",
+		"app.kubernetes.io/part-of":    "amazon-cloudwatch-agent",
+		"app.kubernetes.io/version":    "0.47.0",
+	}
+
+	configYAML, err := os.ReadFile("testdata/prometheus_test.yaml")
+	if err != nil {
+		fmt.Printf("Error getting yaml file: %v", err)
+	}
+
+	t.Run("should return expected prometheus config map", func(t *testing.T) {
+		expectedLabels["app.kubernetes.io/component"] = "amazon-cloudwatch-agent"
+		expectedLabels["app.kubernetes.io/name"] = "test-prometheus-config"
+		expectedLabels["app.kubernetes.io/version"] = "0.0.0"
+
+		expectedData := map[string]string{
+			"prometheus.yaml": `scrape_configs:
+- job_name: cloudwatch-agent
+  scrape_interval: 10s
+  static_configs:
+  - targets:
+    - 0.0.0.0:8888
+`,
+		}
+
+		param := manifests.Params{
+			OtelCol: v1alpha1.AmazonCloudWatchAgent{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "cloudwatch.aws.amazon.com",
+					APIVersion: "v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test",
+					Namespace: "default",
+					UID:       instanceUID,
+				},
+				Spec: v1alpha1.AmazonCloudWatchAgentSpec{
+					Image:      "public.ecr.aws/cloudwatch-agent/cloudwatch-agent:0.0.0",
+					Prometheus: string(configYAML),
+				},
+			},
+		}
+		actual, err := PrometheusConfigMap(param)
+
+		assert.NoError(t, err)
+		assert.Equal(t, "test-prometheus-config", actual.Name)
+		assert.Equal(t, expectedLabels, actual.Labels)
+		assert.Equal(t, expectedData, actual.Data)
+	})
 }
