a98b0c0a01d16f3254bc5bedc2e9361e60256277
Zhihong Lin <zhiholin@amazon.com>
2024-10-16 16:43:17 -0400

    Add chooseServiceName unit test

--- pkg/instrumentation/sdk.go
+++ pkg/instrumentation/sdk.go
@@ -376,10 +376,10 @@ func chooseServiceName(pod corev1.Pod, resources map[string]string, index int) s
 	if name := resources[string(semconv.K8SDaemonSetNameKey)]; name != "" {
 		return name
 	}
-	if name := resources[string(semconv.K8SJobNameKey)]; name != "" {
+	if name := resources[string(semconv.K8SCronJobNameKey)]; name != "" {
 		return name
 	}
-	if name := resources[string(semconv.K8SCronJobNameKey)]; name != "" {
+	if name := resources[string(semconv.K8SJobNameKey)]; name != "" {
 		return name
 	}
 	if name := resources[string(semconv.K8SPodNameKey)]; name != "" {
