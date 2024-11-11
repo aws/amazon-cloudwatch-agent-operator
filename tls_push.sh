9a1248890e3523fa07c6bdc329984894e8e46d59
Okan Kocabalkanli <okanko@amazon.com>
2024-11-11 12:14:38 -0500

    removed unnecessary files

--- tls_push.sh
+++
@@ -1,5 +0,0 @@
-make container
-# docker tag docker.io/aws/cloudwatch-agent-operator:tls  956457624121.dkr.ecr.us-west-2.amazonaws.com/operator:tls 
-docker tag docker.io/aws/cloudwatch-agent-operator:tls  956457624121.dkr.ecr.us-west-2.amazonaws.com/operator:latest 
-# docker push 956457624121.dkr.ecr.us-west-2.amazonaws.com/operator:tls
-docker push 956457624121.dkr.ecr.us-west-2.amazonaws.com/operator:latest
