60e7e5dbfaac6fd6ebc749b4aaddd03ac3a362ae
musa-asad <musaasad@amazon.com>
2025-01-13 19:36:34 -0500

    Specify TLS Ciphers

--- cmd/amazon-cloudwatch-agent-target-allocator/config/config.go
+++ cmd/amazon-cloudwatch-agent-target-allocator/config/config.go
@@ -219,6 +219,14 @@ func ValidateConfig(config *Config) error {
 func (c HTTPSServerConfig) NewTLSConfig(ctx context.Context) (*tls.Config, error) {
 	tlsConfig := &tls.Config{
 		MinVersion: tls.VersionTLS13,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+		},
 	}
 
 	certWatcher, err := certwatcher.New(c.TLSCertFilePath, c.TLSKeyFilePath)
