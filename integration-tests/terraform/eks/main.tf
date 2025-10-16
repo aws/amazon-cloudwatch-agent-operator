b51b6e569b4630dee67922d3a9ebbaa8d079784d
Kaushik Surya <kausyas@amazon.com>
2025-10-16 14:39:25 -0400

    Disable EKS Auto as it is not supported on EKS 1.28

--- integration-tests/terraform/eks/main.tf
+++ integration-tests/terraform/eks/main.tf
@@ -22,6 +22,25 @@ resource "aws_eks_cluster" "this" {
   name     = "cwagent-operator-eks-integ-${module.common.testing_id}"
   role_arn = local.role_arn
   version  = var.k8s_version
+  
+  bootstrap_self_managed_addons = true
+  
+  compute_config {
+    enabled = false
+  }
+  
+  kubernetes_network_config {
+    elastic_load_balancing {
+      enabled = false
+    }
+  }
+  
+  storage_config {
+    block_storage {
+      enabled = false
+    }
+  }
+  
   vpc_config {
     subnet_ids         = module.basic_components.public_subnet_ids
     security_group_ids = [module.basic_components.security_group]
